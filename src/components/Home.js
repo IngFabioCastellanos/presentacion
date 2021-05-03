8b0e24d124a024a3ce61e39172e2445538264800
Fabio Castellanos <fcastellanos782@gmail.com>
2021-05-03 17:26:06 -0500

    correccion en el nombre

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="text-center text-white">
-      <h2 className="pt-20 pb-10 text-5xl font-bold">Fabio Catellanos</h2>
+      <h2 className="pt-20 pb-10 text-5xl font-bold">Fabio Castellanos</h2>
       <h3 className="py-3 text-xl font-semibold">
         Ing. Sistemas y Computación
       </h3>
